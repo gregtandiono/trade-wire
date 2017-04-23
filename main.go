3309e3b20af7547ebc4ca39096e955a638c48b12
gregtandiono <gregtandiono@gmail.com>
2017-04-23 12:56:52 +0700

    added db psql connector

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"database/sql"
 
+	_ "github.com/lib/pq"
+)
+
+func main() {
+}
+
+func dbConnector() (db *sql.DB) {
+	db, err := sql.Open("postgres", "user=postgres password=04120080090 dbname=trade_wire sslmode=verify-full")
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
