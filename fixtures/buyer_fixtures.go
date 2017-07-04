c02f8aea5e744c1b97d2fb1fd2c5fdee4a36d238
Gregory Tandiono <gregtandiono@gmail.com>
2017-07-04 15:05:35 +0700

    removed buyer pic key in buyer fixtures

--- fixtures/buyer_fixtures.go
+++ fixtures/buyer_fixtures.go
@@ -4,25 +4,26 @@ package fixtures
 func BuyerFixtures() map[string]map[string]string {
 	return map[string]map[string]string{
 		"validBuyerRecord": {
-			"id":      "f40e4dd4-f441-428b-8ff3-f893cb176819",
-			"name":    "Japfa Comfeed Indonesia",
-			"address": `Wisma Millenia Lt. 7 \n Jl. MT. Haryono Kav. 16 \n Jakarta 12810, Indonesia \n T.021-2854 5680 (Hunting)`,
-			"pic": `[
-				{
-					"name": "Wahjudi Gunawan", 
-					"position": "Head of Procurement", 
-					"office_number": "+6221566712",
-					"cell_number": "+6281276488",
-					"notes": "Good guy, easy to talk to, prx must be very sharp"
-				},
-				{
-					"name": "Dewi Tjahyono", 
-					"position": "Head of Operations", 
-					"office_number": "+6221566712",
-					"cell_number": "+6281276322",
-					"notes": "Hard to deal with with what she wants"
-				}
-			]`,
+			"id":           "f40e4dd4-f441-428b-8ff3-f893cb176819",
+			"name":         "Japfa Comfeed Indonesia",
+			"address":      `Wisma Millenia Lt. 7 \n Jl. MT. Haryono Kav. 16 \n Jakarta 12810, Indonesia \n T.021-2854 5680 (Hunting)`,
+			"company_type": "buyer",
+			// "pic": `[
+			// 	{
+			// 		"name": "Wahjudi Gunawan",
+			// 		"position": "Head of Procurement",
+			// 		"office_number": "+6221566712",
+			// 		"cell_number": "+6281276488",
+			// 		"notes": "Good guy, easy to talk to, prx must be very sharp"
+			// 	},
+			// 	{
+			// 		"name": "Dewi Tjahyono",
+			// 		"position": "Head of Operations",
+			// 		"office_number": "+6221566712",
+			// 		"cell_number": "+6281276322",
+			// 		"notes": "Hard to deal with with what she wants"
+			// 	}
+			// ]`,
 		},
 		"invalidBuyerRecord": {
 			"name": "",
