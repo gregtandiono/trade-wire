57a61d92052166bf5040380ca8fc149377942bf5
Gregory Tandiono <gregtandiono@gmail.com>
2017-07-04 10:44:05 +0700

    WIP testing variety

--- variety-e2e_test.go
+++ variety-e2e_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"trade-wire/fixtures"
 
 	"github.com/kataras/iris/httptest"
 	uuid "github.com/satori/go.uuid"
@@ -15,6 +16,13 @@ func TestVarietyHandler(t *testing.T) {
 	e.POST("/varieties").
 		WithHeader("Authorization", "Bearer "+aro["token"]).
 		WithJSON(map[string]string{
-			"id": uuid.NewV4().String(),
-		})
+			"id":           uuid.NewV4().String(),
+			"commodity_id": fixtures.CommodityFixtures()["validCommodityRecord"]["id"],
+			"name":         "Australian Standard White (ASW)",
+			"origin":       "australia",
+			"specs":        "Protein min. 10.50%\n Moisture max. 13.50%\n Foreign matter max. 1.00%\n Bug Damage max.1.00%\n Test Weight min. 76kg/HL\n Aflatoxin max. 20ppb\n Vomitoxin max.2ppm",
+		}).
+		Expect().Status(200).JSON().Equal(map[string]string{
+		"message": "variety successfully created",
+	})
 }
