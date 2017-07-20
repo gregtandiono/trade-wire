fc17c3a1a09b4849bedd1432d7438c88212f9cfb
Gregory Tandiono <gregtandiono@gmail.com>
2017-07-20 16:09:30 +0700

    added temp comment / help for tstzrange data type

--- db/init.sql
+++ db/init.sql
@@ -148,3 +148,25 @@ CREATE TABLE IF NOT EXISTS trades(
 
 CREATE TRIGGER update_modified_column
 BEFORE UPDATE ON trades FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+
+-- CREATE TABLE reservation (room int, during tsrange);
+-- INSERT INTO reservation VALUES
+--     (1108, '[2010-01-01 14:30, 2010-01-01 15:30)');
+--
+-- -- Containment
+-- SELECT int4range(10, 20) @> 3;
+--
+-- -- Overlaps
+-- SELECT numrange(11.1, 22.2) && numrange(20.0, 30.0);
+--
+-- -- Extract the upper bound
+-- SELECT upper(int8range(15, 25));
+--
+-- -- Compute the intersection
+-- SELECT int4range(10, 20) * int4range(15, 25);
+--
+-- -- Is the range empty?
+-- SELECT isempty(numrange(1, 5));
+
+-- INSERT INTO tests VALUES
+-- ('fab701fa-6d9b-4ba5-a14d-20a2a726371c','[2017-07-20T16:03:29+07:00, 2017-07-20T16:03:32+07:00]');
