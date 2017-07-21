60f851428e2d76656e1c1c741c015f925d9cf54e
Gregory Tandiono <gregtandiono@gmail.com>
2017-07-21 16:28:00 +0700

    WIP vessel test

--- vessel-e2e_test.go
+++ vessel-e2e_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"testing"
+	"trade-wire/adaptors"
+
+	"github.com/kataras/iris/httptest"
+	uuid "github.com/satori/go.uuid"
+)
+
+func TestVesselHandler(t *testing.T) {
+	app := irisHandler()
+	e := httptest.New(app, t)
+	aro := fetchToken(app, t)
+
+	e.POST("/vessels").
+		WithHeader("Authorization", "Bearer "+aro["token"]).
+		WithJSON(map[string]string{
+			"id":     uuid.NewV4().String(),
+			"name":   "MV Gisela Oldendorf",
+			"beam":   "21.5",
+			"loa":    "20",
+			"draft":  "12m",
+			"status": "loading at loadport",
+		}).Expect().Status(200).JSON().Equal(adaptors.ResponseTemplate("insert:success"))
+}
